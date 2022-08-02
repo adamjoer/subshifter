7fe01d18902f14851ec26cbcb1059ca4fa049085
Adam Harald Jørgensen <58829763+adamjoer@users.noreply.github.com>
2022-08-02 18:31:34 +0200

    Simplify code regarding parseArguments return value

--- main.cpp
+++ main.cpp
@@ -4,12 +4,8 @@ int main(int argc, char *argv[]) {
 
     SubtitleShifter shifter;
 
-    if (auto status = shifter.parseArguments(argc, argv); status != ParseStatus::Continue) {
-        if (status == ParseStatus::Exit)
-            return 0;
-        return 1;
-    }
-
+    if (auto status = shifter.parseArguments(argc, argv); status != ParseStatus::Continue)
+        return (status == ParseStatus::Error) ? 1 : 0;
     shifter.shift();
 
     return 0;
